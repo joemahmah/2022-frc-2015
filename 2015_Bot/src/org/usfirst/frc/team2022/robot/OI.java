be80128d9d833c91dfc4b4697eb544eff42501d8
Nicholas Nusgart <nicknusgart@gmail.com>
2015-01-15 21:22:28 -0600

    Tank Subsystem

--- 2015_Bot/src/org/usfirst/frc/team2022/robot/OI.java
+++ 2015_Bot/src/org/usfirst/frc/team2022/robot/OI.java
@@ -1,13 +1,23 @@
 package org.usfirst.frc.team2022.robot;
 
-import edu.wpi.first.wpilibj.buttons.Button;
-import org.usfirst.frc.team2022.robot.commands.ExampleCommand;
+//import edu.wpi.first.wpilibj.buttons.Button;
+
+import org.usfirst.frc.team2022.robot.controllers.Xbox;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+	public final Xbox xbox;
+	
+	public OI(){
+		xbox = new Xbox(3);
+	}
+	
+	
+	
+	
     //// CREATING BUTTONS
     // One type of button is a joystick button which is any button on a joystick.
     // You create one by telling it which joystick it's on and which button
