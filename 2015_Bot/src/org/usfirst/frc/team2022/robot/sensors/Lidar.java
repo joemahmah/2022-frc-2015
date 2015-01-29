7a30346d63c8cb1967254ea81f68df31ebb6a359
DreadPirateWeslie <grantfinnegan@gmail.com>
2015-01-28 20:58:06 -0600

    added wrapper for lidar, equation for voltage to distance will need adjustment

---
+++ 2015_Bot/src/org/usfirst/frc/team2022/robot/sensors/Lidar.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team2022.robot.sensors;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+
+import org.usfirst.frc.team2022.robot.RobotMap;
+
+public class Lidar extends AnalogInput{
+
+	public Lidar(int channel) {
+		super(channel);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public double getDistance(){
+		//this assumes a linear relationship between voltage and distance, verify if that is correct
+			double voltage = this.getVoltage();
+			double distance = voltage * RobotMap.lidarScalar;
+			return distance
+	}
+	
+	
+}
